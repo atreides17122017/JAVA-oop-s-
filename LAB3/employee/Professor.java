c648cfccec6e516abc815f4b9f3cb3c7eab38135
Tadela Purnima Krishna <tadelapurnima@gmail.com>
2025-08-07 11:19:21 +0530

    Create Professor.java

---
+++ LAB3/employee/Professor.java
@@ -0,0 +1,31 @@
+package employee;
+
+public class Professor extends Employee 
+{
+    protected double bPay;
+    protected String des;
+
+    public Professor(String name, String id, String address, String mail, String mobile, double bPay) 
+    {
+        super(name, id, address, mail, mobile);
+        this.bPay = bPay;
+        this.des = "Professor";
+    }
+
+    @Override
+    public void paySlip() 
+    {
+        double da = 0.97 * bPay;
+        double hra = 0.10 * bPay;
+        double pf = 0.12 * bPay;
+        double staffClub = 0.001 * bPay;
+
+        double gross = bPay + da + hra;
+        double net = gross - (pf + staffClub);
+
+        System.out.println("\nDesignation : " + des);
+        System.out.println("Basic Pay   : " + bPay);
+        System.out.println("Gross Salary: " + gross);
+        System.out.println("Net Salary  : " + net);
+    }
+}
